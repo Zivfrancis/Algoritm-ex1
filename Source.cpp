ecfe4201fe66628352833e93bd401b7906554f7e
mosmatan <matanmo@mta.ac.il>
2024-05-20 16:40:35 +0300

    Work fix 100/100

--- Source.cpp
+++ Source.cpp
@@ -8,15 +8,6 @@ int main()
 {
 	Graph graph;
 
-	//graph.AddEdge(0, 1);
-	//graph.AddEdge(0, 2);
-	//graph.AddEdge(2, 1);
-	//graph.AddEdge(1, 3);
-	//graph.AddEdge(3, 2);
-	//graph.AddEdge(4, 3);
-	//graph.AddEdge(4, 5);
-	//graph.AddEdge(5, 4);
-
 	int n, m;
 
 	cin >> n;
