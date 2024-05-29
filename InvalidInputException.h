d4df27d3f2c50ca61cae006d20abcc15fbe7b5f9
mosmatan <matanmo@mta.ac.il>
2024-05-29 12:04:40 +0300

    add InvalidInputException

---
+++ InvalidInputException.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdlib.h>
+#include <string.h>
+
+class  InvalidInputException
+{
+public:
+	 InvalidInputException();
+	~ InvalidInputException();
+
+private:
+
+};
+
+ InvalidInputException:: InvalidInputException()
+{
+}
+
+ InvalidInputException::~ InvalidInputException()
+{
+}
